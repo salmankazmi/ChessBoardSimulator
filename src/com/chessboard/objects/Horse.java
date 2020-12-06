eb4ed29e072beeacf5fc6bd4834ab5351e814956
slmnkzm@gmail.com <slmnkzm@gmail.com>
2020-12-06 19:54:27 +0530

    typo fix

--- src/com/chessboard/objects/Horse.java
+++ src/com/chessboard/objects/Horse.java
@@ -9,26 +9,21 @@ public class Horse implements ChessPiece {
     @Override
     public List<String> calculatePosition(String rowPosition, int colPosition) {
         int row = Utils.getRowNumber(rowPosition);
-        int col = colPosition;
         List<String> possibleMoves = new ArrayList<>();
 
-        /**
-         * possibilities array for house movement
-         */
+        // possible row and column arrays for horse movement
         int[] rowStepsArray = new int[]{ 2, 2, -2, -2, 1, 1, -1, -1 };
         int[] colStepsArray = new int[]{ -1, 1, 1, -1, 2, -2, 2, -2 };
 
-        /**
-         * horse movement formula
-         * (row + rowStep[i], col + colStep[i])s
-         */
+
+        // horse movement formula = (row + rowStep[i], col + colStep[i])s
         for (int i = 0; i <= 7; i++) {
             int estimatedRowPosition = row + rowStepsArray[ i ];
-            int estimatedColumnPosition = col + colStepsArray[ i ];
+            int estimatedColumnPosition = colPosition + colStepsArray[ i ];
 
             /**
              * validate estimated position.
-             * position must in between 1 to 8 for column and A to H for Row
+             * position must be between 1 to 8 for column and A to H for Row
              */
 
             if ((estimatedRowPosition > 0 && estimatedRowPosition <= 8 ) &&
